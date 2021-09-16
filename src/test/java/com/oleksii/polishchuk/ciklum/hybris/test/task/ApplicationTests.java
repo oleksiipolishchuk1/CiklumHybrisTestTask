2321b1ccca1c48e78b0b1a31dde04e481969f303
Oleksii Polishchuk <cowards_seaward.0g@icloud.com>
2021-09-16 18:49:27 +0300

    Init app

---
+++ src/test/java/com/oleksii/polishchuk/ciklum/hybris/test/task/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleksii.polishchuk.ciklum.hybris.test.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
